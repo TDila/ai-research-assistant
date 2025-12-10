a2b3d6930fd61fdd34ef4ec41c1a6466ee9c1bb5
Tharusha Dilhara <119704521+TDila@users.noreply.github.com>
2025-12-10 23:56:33 +0530

    added text chunking logic with overlap to ingest module

--- src/ingest.py
+++ src/ingest.py
@@ -2,14 +2,30 @@ import fitz
 
 def extract_text_from_pdf(pdfPath: str) -> str:
     doc = fitz.open(pdfPath)
-
+    
     text = ""
     for page in doc:
         text += page.get_text()
-    
+
     doc.close()
     return text
 
-if __name__ in "__main__":
+def chunk_text(text: str, chunk_size: int = 500, overlap: int = 100) -> list:
+    chunks = []
+    start = 0
+    text_length = len(text)
+
+    while start < text_length:
+        end = start + chunk_size
+        chunk = text[start:end]
+        chunks.append(chunk)
+        start = end - overlap
+
+    return chunks
+
+if __name__ == "__main__":
     path = "../data/sample.pdf"
-    print(extract_text_from_pdf(path))
+    text = extract_text_from_pdf(path)
+    chunks = chunk_text(text)
+    print(f"How many chunks we received: {len(chunks)}")
+    print("First chunk preview: \n", chunks[1])
